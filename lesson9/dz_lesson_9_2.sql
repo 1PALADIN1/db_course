b85bc908281bba3dcf4c011cbe01810528416ecc
Ruslan <sonikx94@yandex.ru>
2021-06-29 19:51:25 +0300

    Задание 2

--- lesson9/dz_lesson_9_2.sql
+++ lesson9/dz_lesson_9_2.sql
@@ -7,8 +7,34 @@ USE shop;
 -- с 12:00 до 18:00 функция должна возвращать фразу "Добрый день",
 -- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
 
+-- Проверка функции
 SELECT hello();
 
 -- 2. В таблице products есть два текстовых поля: name с названием товара и description с его описанием.
 -- Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема.
 -- Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
+
+select * from products;
+delete from products
+where id > 7;
+
+-- Проверка триггера на добавление новой записи
+INSERT INTO products (`name`, `description`, price, catalog_id) VALUES (NULL, "Test description", 9567, 1); -- триггер проходит 
+INSERT INTO products (`name`, `description`, price, catalog_id) VALUES ("Test name", NULL, 9567, 1); -- триггер проходит
+INSERT INTO products (`name`, `description`, price, catalog_id) VALUES (NULL, NULL, 9567, 1); -- триггер не проходит
+
+-- Проверка триггера на обновление существующей записи
+-- триггер проходит
+UPDATE products 
+SET `name` = "Test name", `description` = NULL
+WHERE id = 15;
+
+-- триггер проходит
+UPDATE products 
+SET `name` = NULL, `description` = "Test description"
+WHERE id = 15;
+
+-- триггер не проходит
+UPDATE products 
+SET `name` = NULL, `description` = NULL
+WHERE id = 15;
